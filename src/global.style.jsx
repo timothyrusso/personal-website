a3eed1e79161fb44aeada44eda66213d18441020
Timothy Russo <russotimothy@live.it>
2023-02-10 22:26:26 +0100

    feat: clean useless files

---
+++ src/global.style.jsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: hsl(166.6 100% 37.72%);
+        color: black;
+    }
+`
