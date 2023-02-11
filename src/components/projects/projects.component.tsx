c4d463f214cd8869740b3e91f46b32b6b5d8c864
Timothy Russo <russotimothy@live.it>
2023-02-11 22:53:16 +0100

    feat: add sass and project section structure

--- src/components/projects/projects.component.tsx
+++ src/components/projects/projects.component.tsx
@@ -1,9 +1,21 @@
-import { ProjectsComponent } from "./projects.style";
+import { ProjectsComponent, ProjectsGrid, ProjectCard, ProjectTitle } from "./projects.style";
 
 export const Projects = () => {
     return (
         <ProjectsComponent>
             <h2>Projects</h2>
+            <ProjectsGrid>
+                <ProjectCard>
+                    <ProjectTitle>Titolo</ProjectTitle>
+                    <p>Descrizione</p>
+                </ProjectCard>
+                <ProjectCard>
+                    <ProjectTitle>Titolo</ProjectTitle>
+                </ProjectCard>
+                <ProjectCard>
+                    <ProjectTitle>Titolo</ProjectTitle>
+                </ProjectCard>
+            </ProjectsGrid>
         </ProjectsComponent>
     )
 }
--- src/components/projects/projects.style.tsx
+++ src/components/projects/projects.style.tsx
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
 export const ProjectsComponent = styled.section`
-    
+  margin-bottom: 35px;
 `
+
+export const ProjectsGrid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat( auto-fit, minmax(400px, 1fr));
+  list-style-type: none;
+  gap: 20px;
+  padding: 0;
+`
+
+export const ProjectCard = styled.li`
+  border: 2px solid black;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding-left: 20px;
+  &:hover {
+    color: white;
+    cursor: pointer;
+    border-color: white;
+  }
+`
+
+export const ProjectTitle = styled.h3``
