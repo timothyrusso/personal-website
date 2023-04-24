0d9e0e70f3fab88234dcf3194afcd7f60bf38dd6
Timothy Russo <russotimothy@live.it>
2023-04-24 22:45:07 +0200

    feat: update the description

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const projects = [
     title: 'fabrikam-global',
     link: 'https://github.com/timothyrusso/fabrikam-global-webapp-frontend',
     description:
-      'Full stack data entry application built with React.js, TypeScript, Redux, Chakra UI, React Hook Form, Node.js, Express.js and Microsoft SQL',
+      'Full stack data entry application built with React.js, TypeScript, Redux, Chakra UI, React Hook Form, Node.js, Express.js and PostgreSQL',
     id: 1,
   },
   {
